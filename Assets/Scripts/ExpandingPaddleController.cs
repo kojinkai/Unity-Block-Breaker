01dfa7aa4af9d844f980324659a784e2bc4c2250
lewisnixon <lewcfn@gmail.com>
2018-12-19 12:22:41 +0100

    feat(Block): Add an expanding paddle bonus block (#2)
    * add an expanding paddle block, scene 4 and extend paddle controller
    
    * remove unused imports from scripts

---
+++ Assets/Scripts/ExpandingPaddleController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class ExpandingPaddleController : BlockController
+{
+    [SerializeField] PaddleController mainPaddle;
+
+    private void OnCollisionEnter2D()
+    {
+        if (tag == "Breakable")
+        {
+            mainPaddle.ActivateBonusPaddle();
+            HandleHit();
+        }
+    }
+}
--- Assets/Scripts/PaddleController.cs
+++ Assets/Scripts/PaddleController.cs
@@ -1,21 +1,28 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PaddleController : MonoBehaviour
 {
+    // Config params
     [SerializeField] public float screenWidthInUnits = 16f;
     [SerializeField] public float minX = 1f;
     [SerializeField] public float maxX = 15f;
+    [SerializeField] public float bonusPaddleWidth = 1f;
+    [SerializeField] public int bonusPaddleWidthTimer = 5;
+
+
+    private bool bonusPaddleWithActive = false;
 
     // Cached References to Game Objects
     private GameSessionController gameSessionController;
     private BallController ball;
+    private Vector3 initialScale;
 
     void Start()
     {
         gameSessionController = FindObjectOfType<GameSessionController>();
         ball = FindObjectOfType<BallController>();
+        initialScale = transform.localScale;
     }
 
     void Update()
@@ -30,4 +37,21 @@ public class PaddleController : MonoBehaviour
         // if we are in autoplay mode, set in GameSessionController, then track the ball position and not the mouse position
         return gameSessionController.IsAutoPlayEnabled() ? ball.transform.position.x : Input.mousePosition.x / Screen.width * screenWidthInUnits;
     }
+
+    private void SetBonusPaddleWidthInActive() {
+        bonusPaddleWithActive = false;
+        transform.localScale = initialScale;
+    }
+
+    private IEnumerator SetBonusPaddleWidthActive() {
+        bonusPaddleWithActive = true;
+        transform.localScale += new Vector3(bonusPaddleWidth, 0, 0);
+        yield return new WaitForSeconds(bonusPaddleWidthTimer);
+        SetBonusPaddleWidthInActive();
+    }
+
+    public void ActivateBonusPaddle() {
+        StartCoroutine(SetBonusPaddleWidthActive());
+    }
+
 }
